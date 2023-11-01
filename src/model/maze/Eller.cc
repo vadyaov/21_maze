b4bd72b2074832f4b1294946dd819fd93be6491d
vadim <vamyakish@mail.ru>
2023-11-01 22:57:33 +0300

    Eller algo done

--- src/model/maze/Eller.cc
+++ src/model/maze/Eller.cc
@@ -1,60 +1,58 @@
 #include "Eller.h"
 
 #include <random>
-#include <iostream>
 #include <unordered_set>
+#include <algorithm>
 
 namespace maze {
 
+/* The Algorithm
+
+  Note: Assume that there all left-most cells have a left-wall and all right-most
+        cells have a right wall.
+
+  1. Create the first row. No cells will be members of any set
+
+  2. Join any cells not members of a set to their own unique set
+
+  3. Create right-walls, moving from left to right:
+    A. Randomly decide to add a wall or not
+      - If the current cell and the cell to the right are members of the same set,
+        always create a wall between them. (This prevents loops)
+      - If you decide not to add a wall, union the sets to which the current cell
+        and the cell to the right are members.
+
+  4. Create bottom-walls, moving from left to right:
+    A. Randomly decide to add a wall or not. Make sure that each set has at least
+       one cell without a bottom-wall (This prevents isolations)
+      - If a cell is the only member of its set, do not create a bottom-wall
+      - If a cell is the only member of its set without a bottom-wall, do not create a bottom-wall
+
+  5. Decide to keep adding rows, or stop and complete the maze
+    A. If you decide to add another row:
+      a. Output the current row
+      b. Remove all right walls
+      c. Remove cells with a bottom-wall from their set
+      d. Remove all bottom walls
+      e. Continue from Step 2
+
+    B. If you decide to complete the maze
+      a. Add a bottom wall to every cell
+      b. Moving from left to right:
+        - If the current cell and the cell to the right are members of a different set:
+            i.   Remove the right wall
+            ii.  Union the sets to which the current cell and cell to the right are members.
+            iii. Output the final row
+
+*/
 
 bool RandBool() {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<double> dis(0, 1);
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  static std::uniform_real_distribution<double> dis(0, 1);
 
   return dis(gen) < 0.5;
 }
-
-
-/* The Algorithm
-
-    Note: Assume that there all left-most cells have a left-wall and all right-most
-          cells have a right wall.
-
-    1. Create the first row. No cells will be members of any set
-
-    2. Join any cells not members of a set to their own unique set
-
-    3. Create right-walls, moving from left to right:
-      A. Randomly decide to add a wall or not
-        - If the current cell and the cell to the right are members of the same set,
-          always create a wall between them. (This prevents loops)
-        - If you decide not to add a wall, union the sets to which the current cell
-          and the cell to the right are members.
-
-    4. Create bottom-walls, moving from left to right:
-      A. Randomly decide to add a wall or not. Make sure that each set has at least
-         one cell without a bottom-wall (This prevents isolations)
-        - If a cell is the only member of its set, do not create a bottom-wall
-        - If a cell is the only member of its set without a bottom-wall, do not create a bottom-wall
-
-    5. Decide to keep adding rows, or stop and complete the maze
-      A. If you decide to add another row:
-        a. Output the current row
-        b. Remove all right walls
-        c. Remove cells with a bottom-wall from their set
-        d. Remove all bottom walls
-        e. Continue from Step 2
-
-      B. If you decide to complete the maze
-        a. Add a bottom wall to every cell
-        b. Moving from left to right:
-          - If the current cell and the cell to the right are members of a different set:
-              i.   Remove the right wall
-              ii.  Union the sets to which the current cell and cell to the right are members.
-              iii. Output the final row
-
-*/
 
 void ToUniqueSet(std::vector<int>& arr) {
     std::unordered_set<int> uniqueNumbers;
@@ -78,18 +76,10 @@ void ToUniqueSet(std::vector<int>& arr) {
 }
 
 bool Lonely(const std::vector<int>& set, int j) {
-  // lonely on the left side
-  if (j == 0 && set[j + 1] != set[j]) {
-    std::cout << "alone left!\n";
-    return true;
-  }
-  // lonely on the right side
-  if (static_cast<std::size_t>(j) == set.size() - 1 && set[j - 1] != set[j]) {
-    std::cout << "alone right!\n";
-    return true;
-  }
+  if (j == 0 && set[j + 1] != set[j]) return true;
 
-  // lonely middle
+  if (static_cast<std::size_t>(j) == set.size() - 1 && set[j - 1] != set[j]) return true;
+
   return set[j - 1] != set[j] && set[j + 1] != set[j];
 }
 
@@ -100,7 +90,6 @@ bool AloneWithoutWall(const std::vector<int>& set, std::vector<Ceil>& cells, int
   }
 
   pos = j;
-
   while (static_cast<std::size_t>(pos) != 0 && set[pos - 1] == set[pos]) {
     if (cells[i * set.size() + --pos].bottm == false) return false;
   }
@@ -109,17 +98,8 @@ bool AloneWithoutWall(const std::vector<int>& set, std::vector<Ceil>& cells, int
 }
 
 bool CheckBottomWallRule(const std::vector<int>& set, std::vector<Ceil>& cells, int i, int j) {
-  if (Lonely(set, j)) {
-    std::cout << "cell " << i << ":" << j << " alone in its set\n";
-    return false;
-  }
-  
-  // точно есть соседи, но есть ли у них нижние стенки?
-  if (AloneWithoutWall(set, cells, i, j)) {
-    std::cout << "cell " << i << ":" << j << " not alone but only has no bot wall\n";
-    return false;
-  }
-
+  if (Lonely(set, j)) return false;
+  if (AloneWithoutWall(set, cells, i, j)) return false;
   return true;
 }
 
@@ -128,10 +108,11 @@ std::vector<Ceil> Eller::GeneratePerfectMaze(int size) {
 
   // 1. first row creation
   std::vector<int> prev(size, 0);
+
   for (int i = 0; i < size; ++i) {
     std::vector<int> current = prev;
 
-    // remove cells with bottm wall from set
+    // 5.A remove cells with bottm wall from set
     for (int j = 0; i != 0 && j < size; ++j) {
       if (perfect_maze[(i - 1) * size + j].bottm == true)
         current[j] = 0;
@@ -140,59 +121,37 @@ std::vector<Ceil> Eller::GeneratePerfectMaze(int size) {
     // 2. join to unique set
     ToUniqueSet(current);
 
-    std::cout << i << ":";
-    for (auto k : current)
-      std::cout << k << ' ';
-    std::cout << "\n";
+    for (int j = 0; j < size; ++j) {
 
-    // 3. right walls creation
-    for (int j = 0; j < size - 1; ++j) {
-      if (RandBool() || current[j] == current[j + 1]) {
+      // 3. right walls creation
+      if (j == size - 1 || current[j] == current[j + 1] || RandBool()) {
         perfect_maze[i * size + j].right = true;
       } else {
-        /* current[j + 1] = current[j]; */
-        int mutable_value = current[j + 1];
-        for (int k = 0; k < size; ++k) {
-          if (current[k] == mutable_value)
-            current[k] = current[j];
-        }
+        std::replace_if(current.begin(), current.end(), [=](int value) {
+            return value == current[j + 1];
+            },
+            current[j]);
       }
-    }
 
-    std::cout << i << ":";
-    for (auto k : current)
-      std::cout << k << ' ';
-    std::cout << "\n\n";
-
-
-    // 4. bottom walls creation
-    for (int j = 0; j < size; ++j) {
-      if (CheckBottomWallRule(current, perfect_maze, i, j) && RandBool()) {
-        /* std::cout << "place bot  wall" << i << ' ' << j << " " << current[j] << std::endl; */
+      // 4. bottom walls creation
+      if (i == size - 1 || (CheckBottomWallRule(current, perfect_maze, i, j) && RandBool())) {
         perfect_maze[i * size + j].bottm = true;
       }
-    }
 
+    }
 
     prev = current;
 
-    /* if (i == 3) break; */
-
-    /* std::cout << "\n\n\n"; */
   }
 
-  // create right and bot corners
-  for (int i = 0; i < size; ++i) {
-    perfect_maze[i * size + size - 1].right = true;
-    perfect_maze[(size - 1) * size + i].bottm = true;
-  }
-
-
-  // NEED TO FIX THIS (MB NEED TO DO part 3 for right walls) WITH CORRECT UNION
+  // 5.B last row routine
   for (int j = 0; j < size - 1; ++j) {
     if (prev[j + 1] != prev[j]) {
       perfect_maze[(size - 1) * size + j].right = false;
-      prev[j + 1] = prev[j]; // lishnee
+      std::replace_if(prev.begin(), prev.end(), [=](int value) {
+          return value == prev[j + 1];
+          },
+          prev[j]);
     }
   }
 
